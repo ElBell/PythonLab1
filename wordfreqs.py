90c23ef2f8b8664d662c667a95680af311a402a6
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-01 13:25:07 -0400

    Fixed Brian's mistake

--- wordfreqs.py
+++ wordfreqs.py
@@ -1,15 +1,13 @@
 from collections import Counter
 from typing import List
 
-with open("longtext.txt", 'r') as longtext:
-    data: str = longtext.read()
-words: List[str] = data.split()
+words: List[str] = open("longtext.txt", 'r').read().split()
 counter: Counter = Counter(words)
 
-print("Word counts:")
-for letter, count in counter.items():
-    print("{:13}: {:4}".format(letter, count))
+# print("Word counts:")
+# for letter, count in counter.items():
+#     print("{:13}: {:4}".format(letter, count))
 
-print("250 most common:")
-for letter, count in counter.most_common(250):
-    print("{:8}: {:4}".format(letter, count))
+print("250 most common words:")
+for word, count in counter.most_common(250):
+    print("{:8}: {:4}".format(word, count))
